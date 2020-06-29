298b15e115de71dac6d7e7ebfa5a685b040548d3
Hongbin <developer.hb.cai@gmail.com>
2020-06-28 21:34:11 -0400

    add react.js helloworld

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Just for quick reference of starter resources in helloworld pages.
 
 ## deployment reference
 git push -f https://github.com/dev-hbc/frontend-hello-words.git master:gh-pages
+
+## React.js Build 
+```
+The build folder is ready to be deployed.
+You may serve it with a static server:
+
+  serve -s build
+
+Find out more about deployment here:
+
+  bit.ly/CRA-deploy
+```
---
+++ react.js/precache-manifest.fb7cd9d823b875ce10b36b2d3264c3c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b05d8324529ec8445711c2915642cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ecbfcc3badb833eba08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6059468b4f17e8ace471",
+    "url": "/static/js/2.dc9761a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc9761a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ecbfcc3badb833eba08",
+    "url": "/static/js/main.d2a6f0ab.chunk.js"
+  },
+  {
+    "revision": "62f5694a7422e3a2f9d8",
+    "url": "/static/js/runtime-main.d54996da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
